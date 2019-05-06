eaa752f6ca932b4f07714747e12cdd9600d7a32a
ZhuMing <zhuming@mail.hdzuoye.com>
2019-05-06 09:37:51 +0800

    修改main

--- app/src/main/java/com/v/interviewlearn/MainActivity.java
+++ app/src/main/java/com/v/interviewlearn/MainActivity.java
@@ -1,16 +1,9 @@
 package com.v.interviewlearn;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.v.event_dispatch.EventDispatchActivity;
@@ -18,23 +11,13 @@ import com.v.network_architecture.TestNetworkActivity;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextView;
-    private EditText mEditText;
-    private Button mButton;
-    private ImageView mImageView;
-
-
-    private LinearLayout mLinearLayout;
-    private FrameLayout mFrameLayout;
-    private RelativeLayout mRelativeLayout;
-    private ListView mListView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.tv_content);
+        mTextView = findViewById(R.id.tv_content);
 
 //        new Thread(new Runnable() {
 //            @Override
