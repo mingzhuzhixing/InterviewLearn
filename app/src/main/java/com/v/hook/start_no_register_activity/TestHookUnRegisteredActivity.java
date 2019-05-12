5bff9b36805c06dfe80d46f1a6bfdb06b2178c6d
zmg1263368@163.com <19920423zmgh>
2019-05-13 00:26:44 +0800

    hook

---
+++ app/src/main/java/com/v/hook/start_no_register_activity/TestHookUnRegisteredActivity.java
@@ -0,0 +1,15 @@
+package com.v.hook.start_no_register_activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.v.interviewlearn.R;
+
+public class TestHookUnRegisteredActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_hook_un_registered);
+    }
+}
