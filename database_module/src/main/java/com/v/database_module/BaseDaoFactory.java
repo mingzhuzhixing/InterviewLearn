a9a5adcf83d1e8f64df98ed6a70082b4fe35b227
zmg1263368@163.com <zmg1263368@163.com>
2021-09-19 14:13:51 +0800

    修改数据库问题

--- database_module/src/main/java/com/v/database_module/BaseDaoFactory.java
+++ database_module/src/main/java/com/v/database_module/BaseDaoFactory.java
@@ -21,17 +21,17 @@ public class BaseDaoFactory {
     }
 
     private BaseDaoFactory() {
-        sqliteDatabasePath= Environment.getExternalStorageDirectory().getAbsolutePath()+"/dongnao.db";
-        sqLiteDatabase=SQLiteDatabase.openOrCreateDatabase(sqliteDatabasePath,null);
+        sqliteDatabasePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/dongnao.db";
+        sqLiteDatabase = SQLiteDatabase.openOrCreateDatabase(sqliteDatabasePath, null);
     }
 
-    public synchronized <T> BaseDao<T> getBaseDao(Class<T> entityClass){
-        BaseDao<T> baseDao=null;
+    public synchronized <T> BaseDao<T> getBaseDao(Class<T> entityClass) {
+        BaseDao<T> baseDao = null;
 
         try {
-            baseDao=BaseDao.class.newInstance();
+            baseDao = BaseDao.class.newInstance();
 
-            baseDao.init(entityClass,sqLiteDatabase);
+            baseDao.init(entityClass, sqLiteDatabase);
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InstantiationException e) {
--- database_module/src/main/java/com/v/database_module/DatabaseMainActivity.java
+++ database_module/src/main/java/com/v/database_module/DatabaseMainActivity.java
@@ -1,18 +1,29 @@
 package com.v.database_module;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
 import java.util.List;
 
 public class DatabaseMainActivity extends AppCompatActivity {
 
-//    BaseDao baseDao;
+    //    BaseDao baseDao;
     BaseDao<Person> baseDao;
 
     @Override
@@ -26,33 +37,79 @@ public class DatabaseMainActivity extends AppCompatActivity {
 //        baseDao=new BaseDao();
 //        baseDao.init(Person.class,sqLiteDatabase);
 
-        baseDao=BaseDaoFactory.getInstance().getBaseDao(Person.class);
+        //请求权限
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // 如果没有被授予, 直接去请求权限
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        } else {
+            baseDao = BaseDaoFactory.getInstance().getBaseDao(Person.class);
+        }
     }
 
 
     public void insertData(View view) {
-        Person person=new Person("王五",123456L,25);
+        Person person = new Person("王五", 123456L, 25);
 
-        Bitmap bitmap= BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
         person.setPicture(bitmap2Bytes(bitmap));
         baseDao.insert(person);
     }
 
-    private byte[] bitmap2Bytes(Bitmap bitmap){
-        ByteArrayOutputStream baos=new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG,100,baos);
+    private byte[] bitmap2Bytes(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
         return baos.toByteArray();
     }
 
-
-
+    @SuppressLint("SetTextI18n")
     public void searchData(View view) {
-        ImageView imageView=findViewById(R.id.image);
-        Person where=new Person();
-        where.setName("李四");
+        ImageView imageView = findViewById(R.id.image);
+        TextView tvInfo = findViewById(R.id.tv_info);
+        Person where = new Person();
+        where.setName("王五");
         where.setAge(25);
-        List<Person> list=baseDao.queue(where);
-        Bitmap bitmap=BitmapFactory.decodeByteArray(list.get(0).getPicture(),0,list.get(0).getPicture().length);
-        imageView.setImageBitmap(bitmap);
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                List<Person> list = baseDao.queue(where);
+                if (list.isEmpty()) {
+                    return;
+                }
+                Bitmap bitmap = BitmapFactory.decodeByteArray(list.get(0).getPicture(), 0, list.get(0).getPicture().length);
+                imageView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvInfo.setText(list.get(0).name + ": " + list.get(0).age);
+                        imageView.setImageBitmap(bitmap);
+                    }
+                });
+            }
+        }).start();
+    }
+
+    //申请权限结果返回处理
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            boolean isAllGranted = true;
+            // 判断是否所有的权限都已经授予了
+            for (int grant : grantResults) {
+                if (grant != PackageManager.PERMISSION_GRANTED) {
+                    isAllGranted = false;
+                    break;
+                }
+            }
+
+            //判断所有的权限都授予了
+            if (isAllGranted) {
+
+            } else {
+                Toast.makeText(this, "某些权限未开启,请手动开启", Toast.LENGTH_SHORT).show();
+                this.finish();
+            }
+        }
     }
 }
