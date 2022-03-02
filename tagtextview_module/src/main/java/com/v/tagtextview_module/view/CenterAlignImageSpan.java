39ae4383f1f3af6b6f705461ef95f6c3320949b8
zmg1263368@163.com <zmg1263368@163.com>
2022-03-02 18:51:57 +0800

    修改spannablestring

---
+++ tagtextview_module/src/main/java/com/v/tagtextview_module/view/CenterAlignImageSpan.java
@@ -0,0 +1,8 @@
+package com.v.tagtextview_module.view;/**
+ * ClassName: CenterAlignImageSpan
+ * Description: 
+ * @package_name com.v.tagtextview_module.view
+ * @author zhuming
+ * @date 2022/3/2 5:19 下午
+ */public class CenterAlignImageSpan {
+}
