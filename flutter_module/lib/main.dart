09ef8ce4143321dd5016ef976234098743dc24be
zhuming <zmg1263368@163.com>
2022-04-10 20:35:26 +0800

    flutter

--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -1,10 +1,12 @@
+// @dart=2.9
+// 必须在dart文件的第一行,可以加在任何dart文件中
 import 'package:flutter/material.dart';
 import 'package:flutter_module/oschinabody.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- flutter_module/lib/page/setting_page.dart
+++ flutter_module/lib/page/setting_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../utils/event_bus_utils.dart';
+import '../utils/shared_preference_utils.dart';
+
 class SettingPage extends StatefulWidget {
   const SettingPage({Key? key}) : super(key: key);
 
@@ -13,6 +16,14 @@ class _SettingPageState extends State<SettingPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("设置"),
+      ),
+      body: RaisedButton(
+        child: const Text('退出登录'),
+        onPressed: () {
+          SharePreferenceUtils.clearLoginInfo();
+          Navigator.of(context).pop();
+          eventBus.fire(LogoutEvent());
+        },
       ),
     );
   }
