e23ef8dbbcac5417cf87d07f636c181d4894802d
zhuming <zmg1263368@163.com>
2022-04-17 21:54:17 +0800

    banner

--- kotlin/src/main/java/com/v/kotlin/fragment/HomeFragment.kt
+++ kotlin/src/main/java/com/v/kotlin/fragment/HomeFragment.kt
@@ -2,9 +2,12 @@ package com.v.kotlin.fragment
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.stx.xhb.xbanner.XBanner
 import com.v.kotlin.R
 import com.v.kotlin.adapter.HomeAdapter
@@ -14,7 +17,6 @@ import com.v.kotlin.bean.HomeDataBean
 import com.v.kotlin.ivew.IHomeView
 import com.v.kotlin.presenter.HomePresenterImpl
 import com.v.module_recyclerview.decoration.HorizontalDividerItemDecoration
-import com.v.module_recyclerview.decoration.VerticalDividerItemDecoration
 import kotlinx.android.synthetic.main.fragment_home.*
 
 
@@ -64,7 +66,17 @@ class HomeFragment : BaseFragment<HomePresenterImpl>(), IHomeView {
         x_banner.setData(images, titles)
         x_banner.loadImage(object : XBanner.XBannerAdapter {
             override fun loadBanner(banner: XBanner?, model: Any?, view: View, position: Int) {
-                Glide.with(activity!!).load(images[position]).into(view as ImageView)
+                //设置图片圆角角度
+                val roundedCorners = RoundedCorners(20);
+                //通过RequestOptions扩展功能,override:采样率,因为ImageView就这么大,可以压缩图片,降低内存消耗
+                // RequestOptions options = RequestOptions.bitmapTransform(roundedCorners).override(300, 300);
+                val options = RequestOptions.bitmapTransform(roundedCorners);
+                Glide.with(activity!!).load(images[position]).apply(options).into(view as ImageView)
+            }
+        })
+        x_banner.setOnItemClickListener(object : XBanner.OnItemClickListener {
+            override fun onItemClick(banner: XBanner?, model: Any?, view: View?, position: Int) {
+                Toast.makeText(requireContext(), "点击了第" + position + "个", Toast.LENGTH_SHORT).show()
             }
         })
     }
