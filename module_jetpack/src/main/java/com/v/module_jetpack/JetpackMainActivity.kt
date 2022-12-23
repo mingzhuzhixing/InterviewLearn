a1e83350142d00442c2c1e66ad17816cd4b2ffa6
zhuming <zmg1263368@163.com>
2022-12-23 14:27:24 +0800

    jectpack

---
+++ module_jetpack/src/main/java/com/v/module_jetpack/JetpackMainActivity.kt
@@ -0,0 +1,11 @@
+package com.v.module_jetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JetpackMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jetpack_main)
+    }
+}
