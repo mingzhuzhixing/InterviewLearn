29034714c0c2324b527e39835d6337c60444e524
zhuming <zmg1263368@163.com>
2023-03-16 17:08:28 +0800

    AND-0001 viewpager

--- module_viewpager/src/main/java/com/v/module_viewpager/fragment/Fragment2.java
+++ module_viewpager/src/main/java/com/v/module_viewpager/fragment/Fragment2.java
@@ -12,6 +12,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.v.module_viewpager.R;
 import com.v.module_viewpager.adapter.MyViewPagerAdapter;
+import com.v.module_viewpager.widget.ChildViewPager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,10 +24,11 @@ public class Fragment2 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_2, container, false);
 
-        ViewPager viewPager = view.findViewById(R.id.view_pager);
+        ChildViewPager viewPager = view.findViewById(R.id.view_pager);
         List<Fragment> fragments = new ArrayList<>();
         fragments.add(new FragmentSub4());
         fragments.add(new FragmentSub5());
+        fragments.add(new FragmentSub6());
         viewPager.setAdapter(new MyViewPagerAdapter(getChildFragmentManager(), fragments));
         return view;
     }
