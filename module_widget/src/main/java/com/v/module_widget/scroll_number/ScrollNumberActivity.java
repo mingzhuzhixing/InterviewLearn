7f486ce8e3006373526fbdef1ee9b288734fa11d
zhuming <zmg1263368@163.com>
2023-04-24 10:48:22 +0800

    scroll_number

---
+++ module_widget/src/main/java/com/v/module_widget/scroll_number/ScrollNumberActivity.java
@@ -0,0 +1,16 @@
+package com.v.module_widget.scroll_number;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.v.module_widget.R;
+
+public class ScrollNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_number);
+    }
+}
