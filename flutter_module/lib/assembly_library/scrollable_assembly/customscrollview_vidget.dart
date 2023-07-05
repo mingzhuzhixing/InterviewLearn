28aa22d22b023335986b2fd5f1b87b797970176a
zhuming <zmg1263368@163.com>
2023-07-05 23:27:25 +0800

    AND-0001 search

--- flutter_module/lib/assembly_library/scrollable_assembly/customscrollview_vidget.dart
+++ flutter_module/lib/assembly_library/scrollable_assembly/customscrollview_vidget.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_module/bean/item_entity.dart';
+import 'package:flutter_module/page/assemble_page/base_assembly_page.dart';
+import 'package:flutter_module/widget/common_app_bar.dart';
+import 'package:flutter_module/widget/item_button.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 /**
  * CustomScrollView
@@ -21,6 +26,28 @@ import 'package:flutter/material.dart';
     SliverToBoxAdapter	                一个适配器，可以将 RenderBox 适配为 Sliver，后面介绍。
     SliverPersistentHeader	            滑动到顶部时可以固定住，后面介绍。
  */
+class CustomScrollViewHomePage extends StatefulWidget {
+  const CustomScrollViewHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<CustomScrollViewHomePage> createState() => _CustomScrollViewHomePageState();
+}
+
+class _CustomScrollViewHomePageState extends State<CustomScrollViewHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CommonAppBar(context, "CustomScrollView Widget"),
+      body: Column(
+        children: [
+          ItemButton("CustomScrollViewPage", CustomScrollViewPage(), index: 0),
+          ItemButton("CustomScrollViewPage2", CustomScrollViewSearchPage(), index: 1),
+        ],
+      ),
+    );
+  }
+}
+
 class CustomScrollViewPage extends StatefulWidget {
   const CustomScrollViewPage({Key? key}) : super(key: key);
 
@@ -84,6 +111,236 @@ class _CustomScrollViewPageState extends State<CustomScrollViewPage> {
           ),
         ],
       ),
+    );
+  }
+}
+
+/**
+ * CustomScrollView 实现搜索页
+ */
+class CustomScrollViewSearchPage extends StatefulWidget {
+  const CustomScrollViewSearchPage({Key? key}) : super(key: key);
+
+  @override
+  State<CustomScrollViewSearchPage> createState() => _CustomScrollViewSearchPageState();
+}
+
+class _CustomScrollViewSearchPageState extends State<CustomScrollViewSearchPage> {
+  List<String> list = [];
+
+  @override
+  void initState() {
+    super.initState();
+    for (int i = 0; i < 20; i++) {
+      list.add("第一个--->$i");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfff7f7f7),
+      appBar: CommonAppBar(context, "CustomScrollView 实现搜索页"),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              padding: EdgeInsets.only(left: 30.w, right: 30.w),
+              width: 1.sw,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.zero,
+                  topRight: Radius.zero,
+                  bottomLeft: Radius.circular(20.w),
+                  bottomRight: Radius.circular(20.w),
+                ),
+                color: Colors.white,
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: 30.w),
+                  Row(
+                    children: [
+                      Text(
+                        "搜索历史",
+                        style: TextStyle(fontSize: 34.sp, fontWeight: FontWeight.w600),
+                      ),
+                      Spacer(),
+                      Icon(Icons.delete)
+                    ],
+                  ),
+                  SizedBox(height: 30.w),
+                  Wrap(
+                    runSpacing: 10.0,
+                    spacing: 10.0,
+                    children: [
+                      itemWarp("第一个"),
+                      itemWarp("第二个二个二个按钮"),
+                      itemWarp("第三个"),
+                      itemWarp("第四个"),
+                      itemWarp("第五个第五个第"),
+                      itemWarp("第六个"),
+                      itemWarp("第七个第七个第七个第七个"),
+                    ],
+                  ),
+                  SizedBox(height: 30.w),
+                ],
+              ),
+            ),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.only(top: 20.w),
+            sliver: SliverToBoxAdapter(
+              child: Container(
+                width: 1.sw,
+                padding: EdgeInsets.only(left: 20, top: 20.w, bottom: 30.w),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.zero,
+                    bottomLeft: Radius.zero,
+                    topLeft: Radius.circular(20.w),
+                    topRight: Radius.circular(20.w),
+                  ),
+                  color: Colors.white,
+                ),
+                child: Text(
+                  "搜索历史",
+                  style: TextStyle(fontSize: 34.sp, fontWeight: FontWeight.w600),
+                ),
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(childCount: list.length, (ctx, index) {
+              return Container(
+                color: Colors.white,
+                padding: EdgeInsets.only(left: 30.w, bottom: 20.w, right: 30.w),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      child: Image.asset(
+                        "assets/images/icon_cover.png",
+                        width: 148.w,
+                        height: 208.w,
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(8.w),
+                    ),
+                    SizedBox(width: 20.w),
+                    Expanded(
+                      child: SizedBox(
+                        height: 208.w,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "打造超级人脉",
+                              style: TextStyle(
+                                fontSize: 30.sp,
+                                fontWeight: FontWeight.w400,
+                                color: Color(0xff1F1F1F),
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            SizedBox(height: 20.w),
+                            Text(
+                              "他创造了太空歌剧的奇迹，也奠定了赛博朋克的基石，更预见了科技和未来。",
+                              style: TextStyle(
+                                fontSize: 26.sp,
+                                fontWeight: FontWeight.w400,
+                                color: Color(0xff969696),
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            Spacer(),
+                            Text(
+                              "作者：贾斯汀比伯",
+                              style: TextStyle(
+                                fontSize: 24.sp,
+                                fontWeight: FontWeight.w400,
+                                color: Color(0xffb3b3b3),
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            }),
+          ),
+          // SliverToBoxAdapter(
+          //   child: Container(
+          //     width: 1.sw,
+          //     decoration: BoxDecoration(
+          //       borderRadius: BorderRadius.only(
+          //         bottomRight: Radius.zero,
+          //         bottomLeft: Radius.zero,
+          //         topLeft: Radius.circular(20.w),
+          //         topRight: Radius.circular(20.w),
+          //       ),
+          //       color: Colors.white,
+          //     ),
+          //     child: Column(
+          //       crossAxisAlignment: CrossAxisAlignment.start,
+          //       children: [
+          //         Text("搜索历史"),
+          //
+          //         // Expanded(
+          //         //   child: ListView(
+          //         //     children: [
+          //         //       itemWarp("第一个"),
+          //         //       itemWarp("第二个二个二个按钮"),
+          //         //       itemWarp("第三个"),
+          //         //       itemWarp("第四个"),
+          //         //       itemWarp("第五个第五个第"),
+          //         //       itemWarp("第六个"),
+          //         //       itemWarp("第七个第七个第七个第七个"),
+          //         //     ],
+          //         //     shrinkWrap: true,
+          //         //     physics: NeverScrollableScrollPhysics(),
+          //         //   ),
+          //         // )
+          //       ],
+          //     ),
+          //   ),
+          // )
+        ],
+      ),
+    );
+  }
+
+  Widget itemWarp(String text) {
+    return Stack(
+      alignment: Alignment(1.03, -1.3),
+      children: [
+        Container(
+          padding: EdgeInsets.only(left: 30.w, right: 30.w, top: 12.w, bottom: 12.w),
+          child: Text(text),
+          decoration: BoxDecoration(
+            color: Color(0xffF5F3F0),
+            borderRadius: BorderRadius.circular(16.w),
+          ),
+        ),
+        Positioned(
+          child: Container(
+            width: 26.w,
+            height: 26.w,
+            decoration: BoxDecoration(
+              color: Color(0xffDBDAD7),
+              borderRadius: BorderRadius.circular(16.w),
+              border: Border.all(color: Colors.white, width: 2.w),
+            ),
+            child: Icon(Icons.close, color: Colors.white, size: 24.w),
+          ),
+        )
+      ],
     );
   }
 }
