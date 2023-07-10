61e1a98dfea9df7912b6ef16e4cb34749fa62c3f
zhuming <zmg1263368@163.com>
2023-07-10 23:49:27 +0800

    AND-0001 ROW

--- flutter_module/lib/assembly_library/layout_assembly/row_widget.dart
+++ flutter_module/lib/assembly_library/layout_assembly/row_widget.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
 ///Row布局
+/**
+ *  //start：将children放置在主轴的起点；
+    //center：将children放置在主轴的中心；
+    //end：将children放置在主轴的末尾；
+    //spaceAround：将主轴方向上的空白区域均分，使得children之间的空白区域相等，但是首尾child的空白区域为1/2；
+    //spaceBetween：将主轴方向上的空白区域均分，使得children之间的空白区域相等，首尾child都靠近首尾，没有间隙；
+    //spaceEvenly：将主轴方向上的空白区域均分，使得children之间的空白区域相等，包括首尾child；
+ */
 class RowWidgetPage extends StatelessWidget {
   const RowWidgetPage({Key? key}) : super(key: key);
 
@@ -10,38 +18,110 @@ class RowWidgetPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Layout Widget"),
       ),
-      body: Row(
-        //start：将children放置在主轴的起点；
-        //center：将children放置在主轴的中心；
-        //end：将children放置在主轴的末尾；
-        //spaceAround：将主轴方向上的空白区域均分，使得children之间的空白区域相等，但是首尾child的空白区域为1/2；
-        //spaceBetween：将主轴方向上的空白区域均分，使得children之间的空白区域相等，首尾child都靠近首尾，没有间隙；
-        //spaceEvenly：将主轴方向上的空白区域均分，使得children之间的空白区域相等，包括首尾child；
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          MaterialButton(
-            onPressed: () {
-              print("红色按钮");
-            },
-            color: const Color(0xffff0000),
-            child: Text("红色按钮"),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              MaterialButton(
+                onPressed: () {
+                  print("红色按钮");
+                },
+                color: const Color(0xffff0000),
+                child: Text("红色按钮"),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  print("点击蓝色按钮");
+                },
+                color: const Color(0xff000099),
+                child: const Text(
+                  "蓝色按钮",
+                  style: TextStyle(color: Color(0xffffffff)),
+                ),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  print("点击粉色按钮");
+                },
+                color: const Color(0xffee9999),
+                child: Text("粉色按钮"),
+              )
+            ],
           ),
-          MaterialButton(
-            onPressed: () {
-              print("点击蓝色按钮");
-            },
-            color: const Color(0xff000099),
-            child: const Text(
-              "蓝色按钮",
-              style: TextStyle(color: Color(0xffffffff)),
-            ),
+          Stack(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  width: 100,
+                  height: 40,
+                  alignment: Alignment.center,
+                  color: Colors.red,
+                  child: Text('left'),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: 100,
+                  height: 40,
+                  alignment: Alignment.center,
+                  color: Colors.blue,
+                  child: Text('middle'),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  width: 100,
+                  height: 40,
+                  alignment: Alignment.center,
+                  color: Colors.yellow,
+                  child: Text('right'),
+                ),
+              )
+            ],
           ),
-          MaterialButton(
-            onPressed: () {
-              print("点击粉色按钮");
-            },
-            color: const Color(0xffee9999),
-            child: Text("粉色按钮"),
+          SizedBox(height: 20),
+          Stack(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  width: 100,
+                  height: 40,
+                  alignment: Alignment.center,
+                  color: Colors.red,
+                  child: Text('left'),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: 100,
+                  height: 40,
+                  alignment: Alignment.center,
+                  color: Colors.blue,
+                  child: Text('middle'),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Stack(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  width: 100,
+                  height: 40,
+                  alignment: Alignment.center,
+                  color: Colors.red,
+                  child: Text('left'),
+                ),
+              ),
+            ],
           )
         ],
       ),
